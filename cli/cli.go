a09a211d82ec3cc7bdb476de98a9a9a6f0bea058
Juan Carlos Sanchez <juanjcsr@gmail.com>
2019-07-18 15:51:28 -0500

    add client

---
+++ cli/cli.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/juanjcsr/opi_areainfluencia/proto/consignment"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
